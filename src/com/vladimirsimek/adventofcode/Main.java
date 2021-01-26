9afa693e2025e2d5d16413935aac12b8a2207c89
vladimir-simek <minervladik@gmail.com>
2021-01-26 12:23:51 +0100

    1/4

--- src/com/vladimirsimek/adventofcode/Main.java
+++ src/com/vladimirsimek/adventofcode/Main.java
@@ -1,8 +1,24 @@
 package com.vladimirsimek.adventofcode;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+            bw.write("Hello world!");
+            for (int i = 0; i < 2; i++) {
+                bw.newLine();
+                bw.write("Hello world!");
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Successfully written Hello world 3 times.");
     }
 }
