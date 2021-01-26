18b5c3f8eb1d4fdd70ad534152e040e97fdaee52
vladimir-simek <minervladik@gmail.com>
2021-01-26 12:27:06 +0100

    2/4

--- src/com/vladimirsimek/adventofcode/Main.java
+++ src/com/vladimirsimek/adventofcode/Main.java
@@ -19,6 +19,24 @@ public class Main {
             e.printStackTrace();
         }
 
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt", true));
+            bw.newLine();
+            bw.newLine();
+            bw.write("100 LINES INCOMING!");
+            bw.newLine();
+            bw.newLine();
+
+            bw.write("Hello world!");
+            for (int i = 0; i < 100; i++) {
+                bw.newLine();
+                bw.write("Hello world!");
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Successfully written Hello world 3 times.");
     }
 }
